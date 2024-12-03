faa4218d5395b399cdcf51a7e53a585f916439b7
Arthur Henrique <arthurhlsouza@gmail.com>
2024-12-03 10:08:27 -0300

    correção .php

--- usuarios.php
+++ usuarios.php
@@ -47,9 +47,12 @@ if ($action === "register") {
     $data_cadastro = date('Y-m-d H:i:s');
     $stmt->bind_param("sssiss", $nome, $email, $hashedPassword, $nivel_permissao, $status, $data_cadastro);
 
-    if (!$stmt->execute()) {
+    if ($stmt->execute()) {
+        echo json_encode(["success" => true]); // Retorna sucesso
+    } else {
         echo json_encode(["success" => false, "message" => "Erro ao cadastrar usuário.", "data" => []]);
     }
+    exit;
 } elseif ($action === "login") {
     $email = $data->email ?? null;
     $password = $data->password ?? null;
@@ -85,6 +88,7 @@ if ($action === "register") {
     } else {
         echo json_encode(["success" => false, "message" => "E-mail ou senha inválidos.", "data" => []]);
     }
+    exit;
 } elseif ($action === "get_user") {
     if (isset($_SESSION['user_id'])) {
         $user_id = $_SESSION['user_id'];
@@ -102,6 +106,7 @@ if ($action === "register") {
     } else {
         echo json_encode(["success" => false, "message" => "Usuário não logado.", "data" => []]);
     }
+    exit;
 } elseif ($action === "change_password") {
     if (isset($_SESSION['user_id'])) {
         $user_id = $_SESSION['user_id'];
@@ -126,7 +131,9 @@ if ($action === "register") {
                 $update_stmt = $conn->prepare("UPDATE usuarios SET senha = ? WHERE id = ?");
                 $update_stmt->bind_param("si", $hashedPassword, $user_id);
 
-                if (!$update_stmt->execute()) {
+                if ($update_stmt->execute()) {
+                    echo json_encode(["success" => true]); // Retorna sucesso
+                } else {
                     echo json_encode(["success" => false, "message" => "Erro ao alterar a senha.", "data" => []]);
                 }
             } else {
@@ -138,11 +145,15 @@ if ($action === "register") {
     } else {
         echo json_encode(["success" => false, "message" => "Usuário não logado.", "data" => []]);
     }
+    exit;
 }
 
 if ($action === "logout") {
     session_start();
     session_unset();
     session_destroy();
+
+    echo json_encode(["success" => true]); // Apenas retorna o sucesso
+    exit;
 }
 ?>
