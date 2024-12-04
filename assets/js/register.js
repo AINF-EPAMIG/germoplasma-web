a48d170b6a83ca16c21a3defc72798c30f9ad6d2
Arthur Henrique <arthurhlsouza@gmail.com>
2024-12-04 11:54:22 -0300

    remove alert register.js

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,17 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const registerForm = document.getElementById("registerForm");
-    let isSubmitting = false; // Controle para evitar múltiplas submissões
   
     registerForm.addEventListener("submit", async (event) => {
       event.preventDefault(); // Impede o comportamento padrão do formulário
-  
-      if (isSubmitting) {
-        // Se já houver uma submissão em andamento, ignore novas
-        return;
-      }
-  
-      // Marcar que uma submissão está em andamento
-      isSubmitting = true;
   
       // Capturar os dados do formulário
       const nome = document.getElementById("name").value.trim();
@@ -21,12 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
       // Validação básica
       if (!nome || !email || !password) {
         alert("Por favor, preencha todos os campos.");
-        isSubmitting = false; // Permitir novas submissões
         return;
       }
   
+      // Enviar os dados para o servidor
       try {
-        // Enviar os dados para o servidor
         const response = await fetch("https://www.epamig.tech/germoplasma/usuarios.php", {
           method: "POST",
           headers: {
@@ -45,15 +35,10 @@ document.addEventListener("DOMContentLoaded", () => {
         if (data.success) {
           alert("Usuário cadastrado com sucesso!");
           registerForm.reset(); // Limpar o formulário
-        } else {
-          alert(data.message || "Erro ao cadastrar o usuário.");
         }
       } catch (error) {
         console.error("Erro ao cadastrar o usuário:", error);
         alert("Erro ao cadastrar o usuário. Por favor, tente novamente.");
-      } finally {
-        // Libera o formulário para novas submissões
-        isSubmitting = false;
       }
     });
   });
